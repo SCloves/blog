56a5e4d97dafebf2efd04f9a205b3f59fb42438a
Cloves <clovesgtx@gmail.com>
2017-07-31 17:30:51 +0000

    adicionando templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    
+    return render(request, 'blog/post_list.html', {})
 
 # Create your views here.
+
+
